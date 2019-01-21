bbaf832cac5fdc9b18133178e5eab261eb118b60
Drru97 <oleg.dystak@gmail.com>
2019-01-21 11:33:45 +0200

    Remove unnecessary code from controller.

--- src/HealthChecks.Server/Controllers/StatusController.cs
+++ src/HealthChecks.Server/Controllers/StatusController.cs
@@ -15,43 +15,12 @@ namespace HealthChecks.Server.Controllers
             _statusService = statusService;
         }
 
-        public async Task<IActionResult> Status([FromQuery] string command)
+        [HttpGet]
+        public async Task<IActionResult> Status()
         {
-//            var cpu = new System.Diagnostics.PerformanceCounter("Processor", "% Processor Time", "_Total");
-//            var memory = new System.Diagnostics.PerformanceCounter("Memory", "Available MBytes");
-//
-//            var firstCpu = cpu.NextValue();
-//            await Task.Delay(1);
-//            var secondCpu = cpu.NextValue();
-//            
-//            var data = $"CPU: {secondCpu} %\t RAM: {memory.NextValue()} MB.";
-
             var status = await _statusService.GetSystemStatusAsync();
-            var data = ExecuteBashCommand(command);
 
             return Ok(status);
-        }
-
-        private static string ExecuteBashCommand(string command)
-        {
-            command = command.Replace("\"", "\"\"");
-
-            var process = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = "/bin/bash",
-                    Arguments = "-c \"" + command + "\"",
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true
-                }
-            };
-
-            process.Start();
-            process.WaitForExit();
-
-            return process.StandardOutput.ReadToEnd();
         }
     }
 }
